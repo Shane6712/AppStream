5526ca22680d663c65ef14c35226c73e83e77a6b
Shane6712 <53222653+Shane6712@users.noreply.github.com>
2020-03-24 13:01:27 -0400

    Update AppStreamConfigURL.ps1

--- Client/AppStreamConfigURL.ps1
+++ Client/AppStreamConfigURL.ps1
@@ -2,4 +2,4 @@
 $registryPath="HKLM:\Software\Amazon\AppStream Client"
 New-Item -Path "HKLM:\Software\Amazon" -Name "AppStream Client" -Force
 
-New-ItemProperty -Path $registryPath -Name "StartUrl" -Value $URL -PropertyType String -Force | Out-Null
+New-ItemProperty -Path $registryPath -Name "StartUrl" -Value $Url -PropertyType String -Force | Out-Null
